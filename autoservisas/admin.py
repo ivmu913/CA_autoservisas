24b7e410608489d4c3a468e7397b572595324dc9
ivmu913 <ivmu913@gmail.com>
2023-04-26 21:57:59 +0300

    new models_updated admin_gitgnor refresh

--- autoservisas/admin.py
+++ autoservisas/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import CarModel, Car, Service, ServicePrice, Order
 
-# Register your models here.
+class CarADmin(admin.ModelAdmin):
+    list_display = ('client', 'plate_nr', 'car_model', 'vin_number')
+    list_filter = ('client', 'car_model')
+    search_fields = ('client', 'plate_nr', 'vin_number')
+
+admin.site.register(CarModel)
+admin.site.register(Car, CarADmin)
+admin.site.register(Service)
+admin.site.register(ServicePrice)
+admin.site.register(Order)
+# OrderList
+# admin.site.register(OrderList)
--- autoservisas/models.py
+++ autoservisas/models.py
@@ -13,15 +13,23 @@ class CarModel(models.Model):
     def __str__(self):
         return f"{self.brand} - {self.car_model}"
 
+    class Meta:
+        verbose_name = 'Car Model'
+        verbose_name_plural = 'Car Models'
+
 class Car(models.Model):
     car_id = models.AutoField(primary_key=True)
     car_model = models.ForeignKey(CarModel, on_delete=models.SET_NULL, null=True)
     plate_nr = models.CharField(max_length=20)
-    vin_number = models.CharField(max_length=100)
+    vin_number = models.CharField(max_length=17)
     client = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.client} - {self.car_model} - {self.plate_nr} - {self.vin_number}"
+
+    class Meta:
+        verbose_name = 'Car'
+        verbose_name_plural = 'Cars'
 
 
 class Service(models.Model):
@@ -30,6 +38,10 @@ class Service(models.Model):
 
     def __str__(self):
         return f"{self.service_name}"
+
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
 
 class ServicePrice(models.Model):
     id = models.AutoField(primary_key=True)
@@ -40,23 +52,33 @@ class ServicePrice(models.Model):
     def __str__(self):
         return f"{self.service} - {self.price}"
 
+    class Meta:
+        verbose_name = 'Service Price'
+        verbose_name_plural = 'Service Prices'
+
     # TODO - prideti manytomany fielda i admina per atskira methoda
 
 class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
-    order_date = models.DateTimeField(default=datetime.datetime.now())
+    order_date = models.DateTimeField(default=datetime.datetime.now)
     car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField()
     total_price = models.FloatField()
 
     def __str__(self):
-        return f"{self.car} - {self.total_price} - {self.order_date}"
+        return f"{self.car} - {self.service} - {self.total_price} - {self.order_date}"
 
-class OrderList(models.Model):
-    order_list_id = models.AutoField(primary_key=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
-    quantity = models.IntegerField()
-    price = models.FloatField()
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
 
-    def __str__(self):
-        return f"{self.service} - {self.quantity} - {self.price}"
+# class OrderList(models.Model):
+#     order_list_id = models.AutoField(primary_key=True)
+#     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+#
+#     quantity = models.IntegerField()
+#     price = models.FloatField()
+#
+#     def __str__(self):
+#         return f"{self.service} - {self.quantity} - {self.price}"
